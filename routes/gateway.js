2d45bcc7da4085cfcae62f44e4a2db3f7667fa04
Kowus <barnabas@elite-education.org>
2019-08-15 18:06:08 +0000

    rename signup, login handlers

--- routes/gateway.js
+++ routes/gateway.js
@@ -3,22 +3,47 @@ const router = express.Router();
 const debug = require('debug')('accrawan-backend:routes/gateway.js');
 const Gateway = require('../models/gateways');
 const User = require('../models/user');
+const { waterfall } = require('async');
 
 router.post('/create', (req, res) => {
   // async add gateway _id to user on save
-  let { name, model, manufacturer, mac_address } = req.body;
-  let newGateway = new Gateway({
-    name,
-    model,
-    manufacturer,
-    mac_address,
-    added: new Date()
-  });
+  waterfall(
+    [
+      cb => {
+        let { name, model, manufacturer, mac_address } = req.body;
+        let newGateway = new Gateway({
+          name,
+          model,
+          manufacturer,
+          mac_address,
+          added: new Date(),
+          owner: req.user._id
+        });
 
-  newGateway.save((err, gateway) => {
-    if (err) return debug('err');
-    return res.json(gateway);
-  });
+        newGateway.save((err, gateway) => {
+          if (err) return cb(err);
+          return cb(null, gateway);
+        });
+      },
+      (gateway, cb) => {
+        User.findByIdAndUpdate(req.user._id, {
+          $addToSet: {
+            gateways: gateway._id
+          }
+        }).exec(err => {
+          if (err) return cb(err);
+          return cb(null, gateway);
+        });
+      }
+    ],
+    (err, result) => {
+      if (err) {
+        debug(err);
+        return res.json(err);
+      }
+      return res.json(result);
+    }
+  );
 });
 
 router.delete('/delete', (req, res) => {
