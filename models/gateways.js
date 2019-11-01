8608dccf512d67ae9ca9a1ee228a6ea62cd5b705
Kowus <barnabas@elite-education.org>
2019-11-01 02:10:37 +0000

    use mongdb geojson datatype instead of string

--- models/gateways.js
+++ models/gateways.js
@@ -9,9 +9,15 @@ var GatewaySchema = new Schema({
   manufacturer: String,
   model: String,
   location: {
-    x: Number,
-    y: Number,
-    z: Number,
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'], // 'location.type' must be 'Point'
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    },
     description: String
   },
   last_online: Date,
